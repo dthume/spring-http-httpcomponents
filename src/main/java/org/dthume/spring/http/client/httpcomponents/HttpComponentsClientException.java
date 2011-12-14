9739ebd67a28c97ed893ef09e58a8f4500b5767f
David Thomas Hume <dth@dthu.me>
2011-12-14 01:24:41 +0000

    removed constructor

--- src/main/java/org/dthume/spring/http/client/httpcomponents/HttpComponentsClientException.java
+++ src/main/java/org/dthume/spring/http/client/httpcomponents/HttpComponentsClientException.java
@@ -22,11 +22,6 @@ public class HttpComponentsClientException
         super();
     }
 
-    public HttpComponentsClientException(String arg0, Throwable arg1,
-            boolean arg2, boolean arg3) {
-        super(arg0, arg1, arg2, arg3);
-    }
-
     public HttpComponentsClientException(String arg0, Throwable arg1) {
         super(arg0, arg1);
     }
