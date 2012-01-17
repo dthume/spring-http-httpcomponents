cd6b7b57ca24159711f5dc3c15128f5ec22b939c
David Thomas Hume <dth@dthu.me>
2012-01-17 17:50:10 +0000

    Various cleanups

--- src/main/java/org/dthume/spring/http/client/httpcomponents/HttpComponentsClientRequestFactory.java
+++ src/main/java/org/dthume/spring/http/client/httpcomponents/HttpComponentsClientRequestFactory.java
@@ -15,10 +15,20 @@
  */
 package org.dthume.spring.http.client.httpcomponents;
 
+import static org.apache.http.params.CoreConnectionPNames.SO_TIMEOUT;
+
 import java.net.URI;
 
 import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.HttpParams;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.client.ClientHttpRequest;
 import org.springframework.http.client.ClientHttpRequestFactory;
@@ -28,13 +38,24 @@ import org.springframework.http.client.ClientHttpRequestFactory;
  * HttpComponents/client.
  *
  * Accepts an implementation of {@link HttpClient} as an optional constructor
- * argument, defaulting to {@link DefaultHttpClient} if none is supplied.
+ * argument, defaulting to a {@link DefaultHttpClient} using a
+ * {@link ThreadSafeClientConnManager} if none is supplied.
+ * 
+ * This class is based upon the class of the same name from Spring 3.1;
+ * specifically it uses the same set of defaults for the construction
+ * of the default {@code HttpClient} instance.
  *
  * @author dth
  */
 public final class HttpComponentsClientRequestFactory
-    implements ClientHttpRequestFactory {
+    implements ClientHttpRequestFactory, DisposableBean {
 
+    private static final int DEFAULT_MAX_TOTAL_CONNECTIONS = 100;
+
+    private static final int DEFAULT_MAX_CONNECTIONS_PER_ROUTE = 5;
+
+    private static final int DEFAULT_READ_TIMEOUT_MILLISECONDS = (60 * 1000);
+    
     private static final String NULL_CLIENT =
         "HttpClient cannot be null when using single arg constructor";
 
@@ -42,7 +63,38 @@ public final class HttpComponentsClientRequestFactory
 
     /** Create a new request factory with a {@link DefaultHttpClient}. */
     public HttpComponentsClientRequestFactory() {
-        this(new DefaultHttpClient());
+        this(createDefaultClient());
+    }
+    
+    private static HttpClient createDefaultClient() {
+        final HttpClient client =
+                new DefaultHttpClient(createConnectionManager());
+        
+        final HttpParams params = client.getParams();
+        params.setIntParameter(SO_TIMEOUT, DEFAULT_READ_TIMEOUT_MILLISECONDS);
+
+        return client;
+    }
+    
+    private static ClientConnectionManager createConnectionManager() {
+        final SchemeRegistry schemes = new SchemeRegistry();
+        schemes.register(createHttpScheme());
+        schemes.register(createHttpsScheme());
+        
+        final ThreadSafeClientConnManager connManager =
+                new ThreadSafeClientConnManager(schemes);
+        connManager.setMaxTotal(DEFAULT_MAX_TOTAL_CONNECTIONS);
+        connManager.setDefaultMaxPerRoute(DEFAULT_MAX_CONNECTIONS_PER_ROUTE);
+        
+        return connManager;
+    }
+    
+    private static Scheme createHttpScheme() {
+        return new Scheme("http", 80, PlainSocketFactory.getSocketFactory());
+    }
+    
+    private static Scheme createHttpsScheme() {
+        return new Scheme("https", 443, SSLSocketFactory.getSocketFactory());
     }
 
     /** Create a new request factory with the given {@link HttpClient}. */
@@ -56,5 +108,10 @@ public final class HttpComponentsClientRequestFactory
     public ClientHttpRequest createRequest(final URI uri,
             final HttpMethod method) {
         return new HttpComponentsClientHttpRequest(client, uri, method);
+    }
+
+    /** {@inheritDoc} */
+    public void destroy() throws Exception {
+        client.getConnectionManager().shutdown();
     }
 }
